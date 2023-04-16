bc35615bd9f5d5676f5642cedff97354766c2803
Hesenli-Hacaga <hacaga125+1@gmail.com>
2023-04-16 16:54:25 +0400

    Removing boilerplates

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,11 +5,6 @@ export const keys: string[] = [
     // Alphabet keys
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-
-    // // Other keys
-    // 'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
-    // 'Backspace', 'Tab', 'Shift', 'Control', 'Alt', 'Meta', 'CapsLock', 'Enter', 'Space',
-    // 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Insert', 'Delete', 'Home', 'End', 'PageUp', 'PageDown'
 ];
 
 export const levels: { [key: number]: number } = Object.freeze({
